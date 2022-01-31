f70a59517437fed0968c52c4bb5e99f33f3ea343
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-01-31 15:31:05 +0100

    make:crud backoffice

---
+++ src/Form/Movie1Type.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Movie1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('release_date')
+            ->add('duration')
+            ->add('type')
+            ->add('synopsis')
+            ->add('summary')
+            ->add('rating')
+            ->add('poster')
+            ->add('genres')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
