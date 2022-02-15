df0596e4dc1295acf88eb40402cb1d66b782fdf0
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-02-15 11:24:27 +0100

    more tests and custom 404

--- tests/Front/MovieTest.php
+++ tests/Front/MovieTest.php
@@ -2,9 +2,12 @@
 
 namespace App\Tests\Front;
 
+use App\Repository\UserRepository;
+use App\Tests\CoreTest;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
-class MovieTest extends WebTestCase
+class MovieTest extends CoreTest
 {
     public function testMovieHome(): void
     {
@@ -21,9 +24,43 @@ class MovieTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Films, séries TV et popcorn en illimité.');
     }
 
-    public function testRoleUserReviewAdd()
+    /**
+     * Tester les route interdite à un user
+     * 
+     * avec l'annotation dataProvider et un nom de function
+     * cela permet de remplir la valeur de $url avec 
+     * @dataProvider getUrls
+     */
+    public function testRoleUserForbidden($url)
     {
-        // Objectif : tester qu'un utilisateur Connecté puisse voir le bouton "Ajouter un review"
+        // Objectif : tester qu'un utilisateur Connecté NE puisse PAS voir le backoffice
+        
+        // je crée le client HTTP
+        $client = static::createClient();
+
+        // Je veux utilisateur, je demande donc à UserRepository
+        // Pour obtenir un service userRepository, je demande au conteneur de service
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findBy(['email' => 'user@user.com']);
+        //! findBy renvoit un tableau
+        $testuser = $user[0];
+
+        // je demande à mon framework de connecter l'utilisateur
+        $client->loginUser($testuser);
+        
+        //dump($url);
+        $response = $client->request('GET',$url);
+
+        // https://symfony.com/doc/current/testing.html#testing-the-response-assertions
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+        
+    }
+
+
+    public function testRoleAdminBackMovie()
+    {
+        // Objectif : tester qu'un utilisateur Connecté NE puisse PAS voir le backoffice
         
         // je crée le client HTTP
         $client = static::createClient();
@@ -41,31 +78,34 @@ class MovieTest extends WebTestCase
 
         $response = $client->request('GET','/back/movie');
 
-        // Je vérifie que j'ai une réponse HTTP 200 (Succesful)
+        // https://symfony.com/doc/current/testing.html#testing-the-response-assertions
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+
+        /***** Je change d'utilisateur et je teste la même route ******/
+
+        $user = $userRepository->findBy(['email' => 'admin@admin.com']);
+        //! findBy renvoit un tableau
+        $testuser = $user[0];
+
+        // je demande à mon framework de connecter l'utilisateur
+        $client->loginUser($testuser);
+        // si je regarde la route avec un debug:router j'ai '/back/movie/'
+        // je vais voir dans le controller Back\MovieController
+        // j'ai une annotation sur la classe : '/back/movie'
+        // et une annotation sur la route '/'
+        // donc la route connue du FW est '/back/movie/'
+        $response = $client->request('GET','/back/movie/');
+
         $this->assertResponseIsSuccessful();
 
-        // je cherche le bouton
-        // je vais sur la page web, et dans DevTools, sur l'élément > Copy > Copu Selector
-        
-        $this->assertSelectorTextContains('body > div.container.bg-lighttt.pt-5 > div > p > a', 'Ajouter une critique');
     }
 
-    /**
-    * Override PHPUnit fail method
-    * to catch "assertResponse" exceptions
-    * 
-    * @link https://devdocs.io/phpunit~9/fixtures
-    */
-    protected function onNotSuccessfulTest(\Throwable $t): void
+    public function getUrls()
     {
-        // If "assertResponse" is found in the trace, custom message
-        if (strpos($t->getTraceAsString(), 'assertResponse') > 0) {
-            $arrayMessage = explode("\n", $t->getMessage());
-            $message = $arrayMessage[0] . "\n" . $arrayMessage[1];
-            $this->fail($message);
-        }
-
-        // Other Exceptions
-        throw $t;
+        yield ['/back/movie/'];
+        yield ['/back/casting/'];
+        // ajouter d'autre URL si
     }
+
 }
